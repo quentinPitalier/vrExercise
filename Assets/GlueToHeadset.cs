4f59056b856187ea3a7eaf58fb830cc903cd1484
quentinPitalier <quentin.pitalier@gmail.com>
2017-10-17 17:51:53 +0300

    First exercise commit - Pointer & Objects

---
+++ Assets/GlueToHeadset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueToHeadset : MonoBehaviour {
+
+    public GameObject player;
+    public float distanceFromCamera;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z + distanceFromCamera));
+    }
+}
---
+++ Assets/Pointer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gvr.Internal;
+
+public class Pointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = GvrControllerInput.Orientation;
+    }
+}
